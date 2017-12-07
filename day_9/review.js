776e23bd32683bd2e77008b67e2278fa5b78c753
Jacob Tran <jacobtran@gmail.com>
2017-12-06 21:06:37 -0800

    add day 9 files

---
+++ day_9/review.js
@@ -0,0 +1,77 @@
+// good evening fundies!!
+
+// web development
+// learn the fundamentals
+
+// data
+// "Jacob"
+// 38
+// "Summer"
+// true / false = coin
+
+// variable
+// data type
+// integer, float, string, boolean
+
+// int age = 38; // java
+// age = 38 // ruby
+
+// logic, if else
+// loops
+// while, for, do..while
+
+// functions - create a list of instructions, tie to a name
+function morning() {
+  // whole morning routine
+}
+
+morning();
+morning();
+morning();
+morning();
+morning();
+
+// communicating through our code
+// string.split();
+
+function() {} // anonymous function
+
+// events
+// callback function
+setTimeout(morning, 50000); // snooze
+
+setTimeout(function() {
+  // morning routine goes here
+}, 50000); // snooze
+
+// jquery - DOM: Document Object Model
+// javascript communicate with the document or the html or body of the webpage
+// DOM - API
+
+
+// <script src="jquery-3.1.0.js"></script>
+// jquery - $();
+// select things on the page
+$('li').click(function() {
+  // $('li').remove(); all our list items dissapear
+  $(this).remove(); // only the remove the object that threw the event
+});
+
+// jQuery
+// top navigation using jquery
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+//
