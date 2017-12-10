a95129f67eb9c928822c079acb752500fae2817d
Jason <jzntam@gmail.com>
2017-12-10 12:41:14 -0800

    Add Lab Solutions for Day 10 - Code Snippets with Firebase

---
+++ day_10/Code Snippets/firebase_init.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+// THIS IS EXAMPLE ONLY!!!!!
+// Copy and Paste your own Firebase configuration code
+var config = {
+  apiKey: "???",
+  authDomain: "???",
+  databaseURL: "???",
+  projectId: "???",
+  storageBucket: "???",
+  messagingSenderId: "???"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
